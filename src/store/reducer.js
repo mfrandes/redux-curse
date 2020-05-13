79996f2b70cd4ef49d908bfee1b3604f8dcccba6
Mircea <frandesmyrce@gmail.com>
2020-05-13 14:31:03 +0100

    Updating Arrays Immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -29,6 +29,15 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 results: state.results.concat({ id: new Date(), value: state.counter }) //must be concat, push can create un predictable results
+            };
+        case 'DELETE_RESULT':
+            //const updatedArray = state.results.filter(el => true); //this return a new copy for thet array and for each element 
+            const updatedArray = state.results.filter((result) => result.id !== action.resultElId); //this return a new array with new ubjects without the obj we want to remove 
+            console.log(updatedArray);
+            
+            return {
+                ...state,
+                results: updatedArray
             }
         default:
             return state;
