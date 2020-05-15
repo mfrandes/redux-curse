71b28b553f10f987475595417d208bcdf3df7f68
Mircea <frandesmyrce@gmail.com>
2020-05-15 15:20:38 +0100

    utility fuction & leaner switch case

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,24 +1,22 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
 const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    //const updatedArray = state.results.filter(el => true); //this return a new copy for thet array and for each element 
+    // console.log(updatedArray);
+    const updatedArray = state.results.filter((result) => result.id !== action.resultElId); //this return a new array with new ubjects without the obj we want to remove 
+    return updateObject(state, { results: updatedArray });
+}
 const resultsReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: action.result * 2 }) //must be concat, push can create un predictable results
-            };
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) }); //must be concat, push can create un predictable results
         case actionTypes.DELETE_RESULT:
-            //const updatedArray = state.results.filter(el => true); //this return a new copy for thet array and for each element 
-            const updatedArray = state.results.filter((result) => result.id !== action.resultElId); //this return a new array with new ubjects without the obj we want to remove 
-            console.log(updatedArray);
-
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state, action);
         default:
             return state;
     }
