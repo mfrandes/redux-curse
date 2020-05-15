efa4509cafa579cfeaed77547de5ab04cc1341d8
Mircea <frandesmyrce@gmail.com>
2020-05-15 14:17:17 +0100

    restructuring actions ideal for bigger prj for this is overkil

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import { increment, decrement, add, subtract, storeResult, deleteResult } from '../../store/actions/actions';
+import { increment, decrement, add, subtract, storeResult, deleteResult } from '../../store/actions/index';
 class Counter extends Component {
     state = {
         counter: 0
